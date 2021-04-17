3ba60681c84ab274c0b9525b6df13ed63ccb6911
Oleh Podolian <olegvanich@gmail.com>
2021-04-17 12:16:42 +0300

    added example

--- src/main/java/com/oleh/Main.java
+++ src/main/java/com/oleh/Main.java
@@ -5,16 +5,22 @@ import com.oleh.converter.impl.JsonStudentStringConverter;
 import com.oleh.converter.impl.SimpleStudentStringConverter;
 import com.oleh.converter.impl.XmlStudentStringConverter;
 import com.oleh.domain.Student;
+import com.oleh.printer.IStudentPrinter;
+import com.oleh.printer.impl.PrettyStudentPrinter;
 
 public class Main {
     public static void main(String[] args) {
 
         // TODO: 4/17/21  Создать экземпляры объявленных классов и продемонстрировать их работу
 
+        // example
+
         Student student = new Student(1, "Ivan", "Susanin", 3);
 
-        IStudentStringConverter converter = new SimpleStudentStringConverter();
+        IStudentStringConverter converter = new XmlStudentStringConverter();
 
-        System.out.println(converter.convert(student));
+        IStudentPrinter printer = new PrettyStudentPrinter(converter);
+
+        printer.print(student);
     }
 }
