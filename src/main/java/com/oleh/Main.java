5a8f2bd0d94fa85c9d93532aadf5c5af3a32516a
Oleh Podolian <olegvanich@gmail.com>
2021-04-17 12:15:05 +0300

    added todo

--- src/main/java/com/oleh/Main.java
+++ src/main/java/com/oleh/Main.java
@@ -9,6 +9,8 @@ import com.oleh.domain.Student;
 public class Main {
     public static void main(String[] args) {
 
+        // TODO: 4/17/21  Создать экземпляры объявленных классов и продемонстрировать их работу
+
         Student student = new Student(1, "Ivan", "Susanin", 3);
 
         IStudentStringConverter converter = new SimpleStudentStringConverter();
